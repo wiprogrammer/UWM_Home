15dc7662c8783464fc66f9076d9ce9c65d3ec755
dom <dom@wiprogrammer.com>
2018-08-30 10:20:43 -0500

    Update production database

---
+++ app/assets/javascripts/user_home_settings.js
@@ -0,0 +1,33 @@
+
+
+document.addEventListener("turbolinks:load", function () {
+    $("#widgets").sortable({
+
+        update: function (e, ui) {
+
+            Rails.ajax({
+                url: $(this).data("url"),
+                type: "PATCH",
+                data:$(this).sortable('serialize')
+            })
+
+
+        }
+    });
+
+    var $user_form = $("#user_settings_form");
+
+    $("#user_setting_submit").click(function (e) {
+
+        console.log($user_form.attr('action'));
+
+        Rails.ajax({
+            url: $user_form.attr('action'),
+            type: "PATCH",
+            data: $user_form.serialize()
+        });
+        window.location.reload();
+
+    });
+
+});
